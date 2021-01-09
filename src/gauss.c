e6e96419d4ff7b9436a4dd40b2d407aeef6b8aa8
sobczal1 <01159492@pw.edu.pl>
2021-01-09 21:06:18 +0100

    dodanie wyszukiwania elementu największego

--- src/gauss.c
+++ src/gauss.c
@@ -1,5 +1,5 @@
 #include "gauss.h"
-
+#include <stdlib.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -8,6 +8,27 @@ int eliminate(Matrix *mat, Matrix *b){
 	int col = mat->c;
 	int row = mat->r;
 	for(int i = 0; i < row - 1; i++) {
+		double maks = abs(mat->data[i][i]);
+		int index = i;
+		for(int j = i+1; j < row; j++) {
+			if(abs(mat->data[j][i]) > maks) {
+				maks = abs(mat->data[j][i]);
+				index = j;
+			}
+		}
+		if(maks > abs(mat->data[i][i])) {
+			for(int j = 0; j < row; j++) {
+				double temp = mat->data[i][j];
+				mat->data[i][j] = mat->data[index][j];
+				mat->data[index][j] = temp;
+			}
+			 
+			double temp = b->data[i][0];
+			b->data[i][0] = b->data[index][0];
+			b->data[index][0] = temp;
+		}
+
+
 		for(int j = i+1; j < row; j++) {
 			if(mat->data[i][i] == 0) return 1;
 			double wsp = mat->data[j][i]/mat->data[i][i];
